92519738e551271ebd83c22cd67260a62fcda4eb
Hillium <maruruku@stu.csust.edu.cn>
2020-03-22 17:03:06 +0800

    refactor: module provides Stateful and WithPredicate to replace ShouldHandle.

--- module/module.go
+++ module/module.go
@@ -8,24 +8,36 @@ import (
 type Module interface {
 	HandleUpdate(context Context, update tgbotapi.Update, bot *tgbotapi.BotAPI)
 }
-
 type HandleFunc func(update tgbotapi.Update, bot *tgbotapi.BotAPI)
+type StatefulHandleFunc func(ctx Context, update tgbotapi.Update, bot *tgbotapi.BotAPI)
 type trivialModule struct {
-	handleUpdate HandleFunc
-	shouldHandle preds.Predicate
+	handleUpdate StatefulHandleFunc
 }
 
-func (t trivialModule) HandleUpdate(_ Context, update tgbotapi.Update, bot *tgbotapi.BotAPI) {
-	if t.shouldHandle.ShouldHandle(update) {
-		t.handleUpdate(update, bot)
-	}
+func (t trivialModule) HandleUpdate(ctx Context, update tgbotapi.Update, bot *tgbotapi.BotAPI) {
+	t.handleUpdate(ctx, update, bot)
+}
+
+// Stateful warps a stateful function to a Module.
+func Stateful(f StatefulHandleFunc) Module {
+	return trivialModule{handleUpdate: f}
+}
+
+// WithPredicate warps a Module with specified Predicate.
+// The method `handleUpdate` will only be invoked when the Predicate is true.
+func WithPredicate(m Module, p preds.Predicate) Module {
+	return Stateful(func(ctx Context, update tgbotapi.Update, bot *tgbotapi.BotAPI) {
+		if p.ShouldHandle(update) {
+			m.HandleUpdate(ctx, update, bot)
+		}
+	})
 }
 
 // Stateless creates a 'stateless' module.
 // If your state is tiny(which can be captured in a closure), use this.
 func Stateless(handleFunc HandleFunc, condFunc preds.Predicate) Module {
-	return trivialModule{
-		handleUpdate: handleFunc,
-		shouldHandle: condFunc,
-	}
+	handler := Stateful(func(_ Context, update tgbotapi.Update, bot *tgbotapi.BotAPI) {
+		handleFunc(update, bot)
+	})
+	return WithPredicate(handler, condFunc)
 }
