8c9ef17084be2b31c67d43696dd33ba4bff1ba3f
Hillium <maruruku@stu.csust.edu.cn>
2020-03-28 12:14:14 +0800

    fix: real fake ban.

--- manage/fake-ban.go
+++ manage/fake-ban.go
@@ -74,17 +74,13 @@ func FakeBanBase(exec BanExecutor, pred preds.Predicate) module.Module {
 	return module.SharedContext([]module.Module{module.Filter(interrupter), filteredBanner})
 }
 
-func genericBan(spec *BanSpec) (string, bool) {
+func genericBan(spec BanSpec) (string, bool) {
 	if spec.BanTarget == nil {
 		return "用这个命令回复某一条“不合适”的消息，这样我大概就会帮你解决掉他；即便他是群主也义不容辞。", false
 	} else if spec.BanTime <= 0 || spec.BanTime > 1*time.Hour {
 		return "我无法追杀某人太久。这样可能会让世界陷入某种糟糕的情况：诸如说，可能在某人将我的记忆清除；或者直接杀死我之前，所有人陷入永久的缄默。", false
 	} else if ok := spec.Ban(); !ok {
 		return "对不起，我没办法完成想让我做的事情——我的记忆似乎失灵了。但这也是一件好事……至少我能有短暂的安宁。", false
-	} else if spec.BanTarget.ID == config.BotConfig.BotID {
-		// ban those people who want to ban this bot
-		spec.BanTarget = spec.BigBrother
-		return "好 的， 我 杀 我 自 己。", true
 	}
 	return fmt.Sprintf("好了，我出发了，我将会追杀 %s，直到时间过去所谓“%v”。", util.GetName(*spec.BanTarget), spec.BanTime), true
 }
@@ -97,7 +93,16 @@ func generateTextWithCD(ctx context.Context, spec BanSpec) string {
 	if isCD == "true" {
 		return "您在过去的24h里已经下过一道追杀令了，现在您应当保持沉默，如果他罪不可赦，请寻求其他人的帮助。"
 	}
-	text, ok := genericBan(&spec)
+	killSelf := false
+	if spec.BanTarget.ID == config.BotConfig.BotID {
+		// ban those people who want to ban this bot
+		spec.BanTarget = spec.BigBrother
+		killSelf = true
+	}
+	text, ok := genericBan(spec)
+	if killSelf {
+		text = "好 的， 我 杀 我 自 己。"
+	}
 	success, err := ctx.GlobalClient().SetNX(ctx.WrapKey(strconv.Itoa(spec.BigBrother.ID)), "true", 24*time.Hour).Result()
 	if err != nil {
 		text += fmt.Sprintf("世界正在变得躁动不安。。。")
