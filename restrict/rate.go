62350c3b6dc1471dbfbbeff5edbaba43ed888010
icceey <iceclock1037@gmail.com>
2020-12-06 14:34:46 +0800

    update: limit sticker rate

--- restrict/rate.go
+++ restrict/rate.go
@@ -4,6 +4,7 @@ import (
 	"csust-got/context"
 	"csust-got/module"
 	"strconv"
+	"time"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 	"golang.org/x/time/rate"
@@ -11,10 +12,11 @@ import (
 
 var limitMap = make(map[string]*rate.Limiter, 16)
 
-// RateLimit is used to limit rate of user's message.
+// RateLimit 限制消息发送的频率，以防止刷屏.
 func RateLimit(tgbotapi.Update) module.Module {
 	limitHandler := func(ctx context.Context, update tgbotapi.Update, bot *tgbotapi.BotAPI) module.HandleResult {
 		message := update.Message
+		// 特殊消息和私聊不做限流处理
 		if message == nil || message.Chat.IsPrivate() {
 			return module.NextOfChain
 		}
@@ -22,13 +24,19 @@ func RateLimit(tgbotapi.Update) module.Module {
 		chatID := message.Chat.ID
 		key := strconv.FormatInt(chatID, 10) + ":" + strconv.Itoa(userID)
 		if limiter, ok := limitMap[key]; ok {
-			if limiter.Allow() {
+			// 普通消息消耗一个令牌
+			if message.Sticker == nil && limiter.Allow() {
 				return module.NextOfChain
 			}
+			// Sticker消耗两个令牌
+			if message.Sticker != nil && limiter.AllowN(time.Now(), 2) {
+				return module.NextOfChain
+			}
+			// 令牌不足撤回消息
 			_, _ = bot.DeleteMessage(tgbotapi.NewDeleteMessage(chatID, message.MessageID))
 			return module.NoMore
 		}
-		limitMap[key] = rate.NewLimiter(1, 15)
+		limitMap[key] = rate.NewLimiter(1, 20)
 		return module.NextOfChain
 	}
 	return module.Filter(limitHandler)
