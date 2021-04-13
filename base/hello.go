64063d8cc8caefdb326a080402fdb01fe687b078
icceey <iceclock1037@gmail.com>
2021-04-13 20:11:26 +0800

    update: forward retry

--- base/hello.go
+++ base/hello.go
@@ -100,9 +100,13 @@ func Forward(m *Message) {
 		Chat: m.Chat,
 	}
 
-	if _, err := config.GetBot().Forward(m.Chat, forwardMsg); err != nil {
-		const msgFmt = "我们试图找到那条消息[%d]，但是它已经永远的消失在了历史记录的长河里，对此我们深表遗憾。诀别来的总是那么自然，在你注意到时发现已经消失，希望你能珍惜现在的热爱。"
-		util.SendReply(m.Chat, fmt.Sprintf(msgFmt, historyID), m)
+	if _, err := config.GetBot().Forward(m.Chat, forwardMsg); err != nil && command.Argc() == 0 {
+		// retry
+		forwardMsg.ID = rand.Intn(m.ID) + 1
+		if _, err := config.GetBot().Forward(m.Chat, forwardMsg); err != nil {
+			const msgFmt = "我们试图找到那条消息[%d]，但是它已经永远的消失在了历史记录的长河里，对此我们深表遗憾。诀别来的总是那么自然，在你注意到时发现已经消失，希望你能珍惜现在的热爱。"
+			util.SendReply(m.Chat, fmt.Sprintf(msgFmt, forwardMsg.ID), m)
+		}
 	}
 }
 
