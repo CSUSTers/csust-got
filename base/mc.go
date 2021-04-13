d21b5c833582e6237a9d339b848ce3b72c2275fd
icceey <iceclock1037@gmail.com>
2021-04-13 21:07:23 +0800

    fix: fix text

--- base/mc.go
+++ base/mc.go
@@ -48,7 +48,7 @@ func MC(m *Message) {
 	text := "本群大水怪名单:\n"
 	// rankCN := []string{"零", "一", "二", "三", "四", "wu", "六", "七", "八", "九", "十"}
 	text += fmt.Sprintf("第一名：'%v'！他的一生，是龙王的一生，他把有限的生命贡献在了无限的发送 %v 上，24h内 %v 数量高达 %v 条！群友因为他感受到这个群还有活人，我们把最热烈 fake_ban 送给他，让他在新的一天里享受快乐的退休时光吧！\n",
-		data[0].Name, msgType, data[0].Value, msgType)
+		data[0].Name, msgType, msgType, data[0].Value)
 	if len(data) > 1 {
 		text += fmt.Sprintf("第二名：'%v'！他用上洪荒之力，在24h内水了 %v 条 %v ，这个数字证明了它他的决心，虽然没能夺冠，让我们仍旧把掌声送给他！\n",
 			data[1].Name, data[1].Value, msgType)
