d2a49ace8a26c33489d64bc6b695b65cbe824b53
Anthony-Hoo <tonyhoo2000@hotmail.com>
2023-02-16 14:33:48 +0800

    fix: 修正某些情况下不会返回凯瑟琳回复的问题

--- base/genshin_voice.go
+++ base/genshin_voice.go
@@ -3,6 +3,7 @@ package base
 import (
 	"csust-got/config"
 	"csust-got/log"
+	"csust-got/util"
 	"encoding/json"
 	"fmt"
 	"go.uber.org/zap"
@@ -20,29 +21,30 @@ type genShinVoice struct {
 
 // GetVoice 从api服务器拿到语音的url以及其他信息，并发送为tg的voice信息
 func GetVoice(ctx Context) error {
+	m := ctx.Message()
 	data := genShinVoice{}
 	serverAddress := config.BotConfig.GenShinConfig.ApiServer
-	resp, err := http.Get(serverAddress)
+	resp, err := http.Get(serverAddress + "/GenShin/GetVoice")
 	if err != nil {
 		log.Error("api server error", zap.Error(err))
-		err := ctx.Reply("凯瑟琳: \n 异常……", nil)
+		util.SendReply(m.Chat, "凯瑟琳: \n 异常……", m)
 		return err
 	}
 	body, _ := io.ReadAll(resp.Body)
 	if resp.StatusCode != 200 {
 		log.Error("api server response", zap.Int("status", resp.StatusCode), zap.String("body", string(body)))
-		err := ctx.Reply("凯瑟琳: \n 重试……", nil)
+		util.SendReply(m.Chat, "凯瑟琳: \n 重试……", m)
 		return err
 	}
 	err = json.Unmarshal(body, &data)
 	if err != nil {
 		log.Error("json serialization failed", zap.Error(err), zap.String("body", string(body)))
-		err := ctx.Reply("凯瑟琳: \n 超时……", nil)
+		util.SendReply(m.Chat, "凯瑟琳: \n 超时……", m)
 		return err
 
 	}
 	audioCaption := fmt.Sprintf("%s \n\n #%s  %s", data.Text, data.Character, data.Topic)
 	voice := Voice{File: FromURL(data.Audio), Caption: audioCaption}
-	_, err = voice.Send(ctx.Bot(), ctx.Recipient(), nil)
+	_, err = voice.Send(config.BotConfig.Bot, m.Chat, nil)
 	return err
 }
