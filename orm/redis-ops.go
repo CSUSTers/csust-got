e69dfdc5d8b1c9a672e832ab3ea2c514a173dcf7
Hugefiver <i@iruri.moe>
2023-02-21 03:25:37 +0000

    fix new redis lib compitible problem

--- orm/redis-ops.go
+++ orm/redis-ops.go
@@ -1,6 +1,7 @@
 package orm
 
 import (
+	"context"
 	"errors"
 	"time"
 
@@ -10,7 +11,8 @@ import (
 // GetBool gets a bool type value to a key in the redis storage.
 // This function will call WrapKey, so you needn't warp your key.
 func GetBool(key string) (bool, error) {
-	enable, err := rc.Get(key).Int()
+	// TODO: replace ctx with real ctx
+	enable, err := rc.Get(context.TODO(), key).Int()
 	if errors.Is(err, redis.Nil) {
 		return false, nil
 	}
@@ -24,7 +26,8 @@ func WriteBool(key string, value bool, expiration time.Duration) error {
 	if value {
 		newI = 1
 	}
-	return rc.Set(key, newI, expiration).Err()
+	// TODO: replace ctx with real ctx
+	return rc.Set(context.TODO(), key, newI, expiration).Err()
 }
 
 // ToggleBool toggles(negative) a bool type value to a key in the redis storage.
@@ -39,7 +42,8 @@ func ToggleBool(key string) error {
 
 // GetTTL get key expire duration.
 func GetTTL(key string) (time.Duration, error) {
-	sec, err := rc.TTL(key).Result()
+	// TODO: replace ctx with real ctx
+	sec, err := rc.TTL(context.TODO(), key).Result()
 	if err != nil || sec < 0 {
 		return 0, err
 	}
