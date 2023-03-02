2f7ddcd092326f19f565eedae1f6eae99219f3b7
Hugefiver <i@iruri.moe>
2023-03-02 08:46:54 +0000

    fix

--- attachment/voiceGen/start_docker_container.sh
+++ attachment/voiceGen/start_docker_container.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 app="voice_gen"
-docker build -t ${app} .
-docker run -d -p 37332:8000 voice_gen
+tag="voice_gen"
+
+docker build -t ${tag} .
+
+# stop and remove running docker container
+if docker ps -a | grep -q $app; then
+  docker stop $app
+  
+  if [ $? -eq 0 ]; then
+    docker rm $container_name
+  else
+    echo "cannot stop container, exit."
+    exit 255
+  fi
+fi
+
+docker run -d -p 37332:8000 --name $app $tag
