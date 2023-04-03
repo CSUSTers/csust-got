31d99249f774b32e523b4739766dfe4f781c0bbb
Anthony-Hoo <tonyhoo2000@hotmail.com>
2023-04-03 16:20:51 +0800

    fix: (/qiuchat) 减少输入困难，命令从chatqiu改为qiuchat

--- main.go
+++ main.go
@@ -132,7 +132,7 @@ func registerBaseHandler(bot *Bot) {
 
 	bot.Handle("/chat", chat.GPTChat, whiteMiddleware)
 	bot.Handle("/chats", chat.GPTChatWithStream, whiteMiddleware)
-	bot.Handle("/chatqiu", chat.Cust, whiteMiddleware)
+	bot.Handle("/qiuchat", chat.Cust, whiteMiddleware)
 }
 
 func registerRestrictHandler(bot *Bot) {
