d21b9f63142f6aec9aa15cdd89f2d8317faef12b
Hugefiver <i@iruri.moe>
2023-12-29 19:00:27 +0000

    add `TLDsAscii`

--- util/urlx/tlds_test.go
+++ util/urlx/tlds_test.go
@@ -9,9 +9,18 @@ import (
 )
 
 func TestTldsRegex(t *testing.T) {
-	patt := regexp.MustCompile(fmt.Sprintf(`^%[1]s$`, TLDRegex))
+	tldsPatt := regexp.MustCompile(fmt.Sprintf(`^%[1]s$`, TLDRegex))
+	tldsAsciiPatt := regexp.MustCompile(fmt.Sprintf(`^%[1]s$`, TLDAsciiRegex))
 
-	for _, s := range TLDs {
-		assert.True(t, patt.MatchString(s), "TLD regex cannot match '%s'", s)
-	}
+	t.Run("TLDRegex should match all `TLDs`", func(t *testing.T) {
+		for _, s := range TLDs {
+			assert.True(t, tldsPatt.MatchString(s), "TLD regex cannot match '%s'", s)
+		}
+	})
+
+	t.Run("TLDAsciiRegex should match all `TLDsAscii`", func(t *testing.T) {
+		for _, s := range TLDsAscii {
+			assert.True(t, tldsAsciiPatt.MatchString(s), "TLD regex cannot match '%s'", s)
+		}
+	})
 }
