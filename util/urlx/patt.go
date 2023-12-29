65bcc89e52443c14fca483a124ac4996dbb64161
Hugefiver <i@iruri.moe>
2023-12-29 13:10:45 +0800

    add url regex pattern unit tests

--- util/urlx/patt.go
+++ util/urlx/patt.go
@@ -6,12 +6,30 @@ import (
 	"strings"
 )
 
-const regexTempl = `(?mi)\b(?:(?P<schema>https?)://)?(?:(?P<domain>(?:[\w\d~-]+\.)+(?P<tld>(?:%[1]s)))(?:\:(?P<port>\d{1,5}))?(?P<path>(?:/[^\s\?&:()$!]*)*))(?P<query>\?(?:[^\s()^$!]*))?`
+const regexTempl = `(?mi)(?:^|\s)(?P<url>(?:(?P<schema>https?)://)?(?:(?P<domain>(?:[\w\d~-]+\.)+(?P<tld>(?:%[1]s)))(?:\:(?P<port>\d{1,5}))?(?P<path>(?:/[^\s\?&:()$!"'#]*)*))(?P<query>\?(?:[^\s()^$!"'#]*))?(?P<hash>#(?:[^\s()^$!"']*))?)`
 
 // Patt is alias to [`UrlPatt`]
 var Patt = regexp.MustCompile(fmt.Sprintf(regexTempl, strings.Join(TLDs, "|")))
 
 var UrlPatt = Patt
+
+func SubmatchGroupString(s string, matchIndexes []int, groupIdx int) string {
+	if groupIdx < 0 || groupIdx >= len(matchIndexes)/2 {
+		return ""
+	}
+	if matchIndexes[groupIdx*2] < 0 || matchIndexes[groupIdx*2+1] < 0 {
+		return ""
+	}
+	return s[matchIndexes[groupIdx*2]:matchIndexes[groupIdx*2+1]]
+}
+
+func SubmatchGroupStringFromName(p *regexp.Regexp, s string, matchIndexes []int, groupName string) string {
+	groupIdx := p.SubexpIndex(groupName)
+	if groupIdx < 0 {
+		return ""
+	}
+	return SubmatchGroupString(s, matchIndexes, groupIdx)
+}
 
 func init() {
 	UrlPatt.Longest()
--- util/urlx/patt_test.go
+++ util/urlx/patt_test.go
@@ -42,18 +42,131 @@ func TestUrlRegexSubmatch(t *testing.T) {
 abc https://example.com
 测试 http://example.com/echo 测试
 测试 example.com?abc=def 测试
+	https://example.com/echo1/echo2?text1=hello#here?is=world"
 `
 
 		ms := Patt.FindAllStringSubmatchIndex(text, -1)
-		if len(ms) != 3 {
-			t.Errorf("len(matches) = %d, want %d", len(ms), 3)
-		}
-		s1 := text[ms[0][0]:ms[0][1]]
-		s2 := text[ms[1][0]:ms[1][1]]
-		s3 := text[ms[2][0]:ms[2][1]]
 		t.Log(ms)
+		if len(ms) != 4 {
+			t.Errorf("len(matches) = %d, want %d", len(ms), 4)
+		}
+
+		s1 := text[ms[0][2]:ms[0][3]]
+		s2 := text[ms[1][2]:ms[1][3]]
+		s3 := text[ms[2][2]:ms[2][3]]
+		s4 := text[ms[3][2]:ms[3][3]]
+
 		assert.Equal(t, "https://example.com", s1)
 		assert.Equal(t, "http://example.com/echo", s2)
 		assert.Equal(t, "example.com?abc=def", s3)
+		assert.Equal(t, "https://example.com/echo1/echo2?text1=hello#here?is=world", s4)
+	})
+
+	t.Run("test-2", func(t *testing.T) {
+		text := `
+测试https://example.com (不匹配)
+test+http://example.com/echo (不匹配)
+^example.com?abc=def (不匹配)
+`
+
+		ms := Patt.FindAllStringSubmatchIndex(text, -1)
+		if len(ms) != 0 {
+			t.Errorf("len(matches) = %d, want %d", len(ms), 0)
+		}
+	})
+
+	t.Run("test-3", func(t *testing.T) {
+		text := `
+测试命名捕获组 https://example.com
+测试命名捕获组 http://example.com/echo 测试
+测试命名捕获组 example.com?abc=def 测试
+测试命名捕获组 example.com:8080 测试
+测试命名捕获组 https://example.com/echo?text1=hello&text2=#here
+测试命名捕获组 https://example.com/echo?text1=hello&text2=#here?foo=bar
+`
+
+		ms := Patt.FindAllStringSubmatchIndex(text, -1)
+		t.Log(ms)
+		if len(ms) != 6 {
+			t.Errorf("len(matches) = %d, want %d", len(ms), 6)
+		}
+
+		m1 := ms[0]
+		m2 := ms[1]
+		m3 := ms[2]
+		m4 := ms[3]
+		m5 := ms[4]
+		m6 := ms[5]
+
+		urlIdx := Patt.SubexpIndex("url")
+		schemaIdx := Patt.SubexpIndex("schema")
+		domainIdx := Patt.SubexpIndex("domain")
+		tldIdx := Patt.SubexpIndex("tld")
+		portIdx := Patt.SubexpIndex("port")
+		pathIdx := Patt.SubexpIndex("path")
+		queryIdx := Patt.SubexpIndex("query")
+		hashIdx := Patt.SubexpIndex("hash")
+
+		t.Log(urlIdx, schemaIdx, domainIdx, tldIdx, portIdx, pathIdx, queryIdx, hashIdx)
+
+		// match 1
+		assert.Equal(t, "https://example.com", SubmatchGroupString(text, m1, urlIdx))
+		assert.Equal(t, "https", SubmatchGroupString(text, m1, schemaIdx))
+		assert.Equal(t, "example.com", SubmatchGroupString(text, m1, domainIdx))
+		assert.Equal(t, "com", SubmatchGroupString(text, m1, tldIdx))
+		assert.Equal(t, "", SubmatchGroupString(text, m1, portIdx))
+		assert.Equal(t, "", SubmatchGroupString(text, m1, pathIdx))
+		assert.Equal(t, "", SubmatchGroupString(text, m1, queryIdx))
+		assert.Equal(t, "", SubmatchGroupString(text, m1, hashIdx))
+
+		// match 2
+		assert.Equal(t, "http://example.com/echo", SubmatchGroupString(text, m2, urlIdx))
+		assert.Equal(t, "http", SubmatchGroupString(text, m2, schemaIdx))
+		assert.Equal(t, "example.com", SubmatchGroupString(text, m2, domainIdx))
+		assert.Equal(t, "com", SubmatchGroupString(text, m2, tldIdx))
+		assert.Equal(t, "", SubmatchGroupString(text, m2, portIdx))
+		assert.Equal(t, "/echo", SubmatchGroupString(text, m2, pathIdx))
+		assert.Equal(t, "", SubmatchGroupString(text, m2, queryIdx))
+		assert.Equal(t, "", SubmatchGroupString(text, m2, hashIdx))
+
+		// match 3
+		assert.Equal(t, "example.com?abc=def", SubmatchGroupString(text, m3, urlIdx))
+		assert.Equal(t, "", SubmatchGroupString(text, m3, schemaIdx))
+		assert.Equal(t, "example.com", SubmatchGroupString(text, m3, domainIdx))
+		assert.Equal(t, "com", SubmatchGroupString(text, m3, tldIdx))
+		assert.Equal(t, "", SubmatchGroupString(text, m3, portIdx))
+		assert.Equal(t, "", SubmatchGroupString(text, m3, pathIdx))
+		assert.Equal(t, "?abc=def", SubmatchGroupString(text, m3, queryIdx))
+		assert.Equal(t, "", SubmatchGroupString(text, m3, hashIdx))
+
+		// match 4
+		assert.Equal(t, "example.com:8080", SubmatchGroupString(text, m4, urlIdx))
+		assert.Equal(t, "", SubmatchGroupString(text, m4, schemaIdx))
+		assert.Equal(t, "example.com", SubmatchGroupString(text, m4, domainIdx))
+		assert.Equal(t, "com", SubmatchGroupString(text, m4, tldIdx))
+		assert.Equal(t, "8080", SubmatchGroupString(text, m4, portIdx))
+		assert.Equal(t, "", SubmatchGroupString(text, m4, pathIdx))
+		assert.Equal(t, "", SubmatchGroupString(text, m4, queryIdx))
+		assert.Equal(t, "", SubmatchGroupString(text, m4, hashIdx))
+
+		// match 5
+		assert.Equal(t, "https://example.com/echo?text1=hello&text2=#here", SubmatchGroupString(text, m5, urlIdx))
+		assert.Equal(t, "https", SubmatchGroupString(text, m5, schemaIdx))
+		assert.Equal(t, "example.com", SubmatchGroupString(text, m5, domainIdx))
+		assert.Equal(t, "com", SubmatchGroupString(text, m5, tldIdx))
+		assert.Equal(t, "", SubmatchGroupString(text, m5, portIdx))
+		assert.Equal(t, "/echo", SubmatchGroupString(text, m5, pathIdx))
+		assert.Equal(t, "?text1=hello&text2=", SubmatchGroupString(text, m5, queryIdx))
+		assert.Equal(t, "#here", SubmatchGroupString(text, m5, hashIdx))
+
+		// match 6
+		assert.Equal(t, "https://example.com/echo?text1=hello&text2=#here?foo=bar", SubmatchGroupString(text, m6, urlIdx))
+		assert.Equal(t, "https", SubmatchGroupString(text, m6, schemaIdx))
+		assert.Equal(t, "example.com", SubmatchGroupString(text, m6, domainIdx))
+		assert.Equal(t, "com", SubmatchGroupString(text, m6, tldIdx))
+		assert.Equal(t, "", SubmatchGroupString(text, m6, portIdx))
+		assert.Equal(t, "/echo", SubmatchGroupString(text, m6, pathIdx))
+		assert.Equal(t, "?text1=hello&text2=", SubmatchGroupString(text, m6, queryIdx))
+		assert.Equal(t, "#here?foo=bar", SubmatchGroupString(text, m6, hashIdx))
 	})
 }
