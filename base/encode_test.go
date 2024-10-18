20e4f81862fcaa6a306a401f852a74e004a483ed
Hugefiver <i@iruri.moe>
2024-10-18 15:55:01 +0800

    impl hoocoder

--- base/encode_test.go
+++ base/encode_test.go
@@ -11,40 +11,17 @@ func Test_encode(t *testing.T) {
 		args string
 		want string
 	}{
-		{"hugefiver", "hugefiver"},
-		{"hugeer", "hugeer"},
-		{"huger", "hugeF**Ker"},
-		{"", "HUGEFIVER"},
-		{"even", "hugevener"},
-		{"ray", "hugerayer"},
-		{"wu", "hugewuer"},
-		{"e", "hugeF**Ker"},
-		{"en", "hugener"},
-		{"py", "hugepier"},
+		{"", "h0oOo"},
+		{"0", "h0oOo"},
+		{"Hoo", "h0oOo"},
+		{"h0o0o00", "h0oOo"},
+		{"FAKER", "h0oFAKEROo"},
+		{"h0o0o0OFAKER", "h0oFAKEROo"},
+		{"FAKERo0oO0", "h0oFAKEROo"},
+		{"h0o0o0OFAKERo0oO0", "h0oFAKEROo"},
 	}
 	for _, tt := range tests {
 		got := hooEncode(tt.args)
 		require.Equalf(t, tt.want, got, "encode(%s)", tt.args)
-	}
-}
-
-func Test_decode(t *testing.T) {
-	tests := []struct {
-		args string
-		want string
-	}{
-		{"hugefiver", "hugefiver"},
-		{"hugeer", "hugeFAKEr"},
-		{"huger", "hugeFAKEr"},
-		{"", "hugeFAKEr"},
-		{"even", "hugeFAKEr"},
-		{"hugehugefiver", "hugefiver"},
-		{"hugefiverer", "hugefiver"},
-		{"hugehugefiverer", "hugefiver"},
-		{"hugehugeer", "hugeer"},
-	}
-	for _, tt := range tests {
-		got := hooDecode(tt.args)
-		require.Equalf(t, tt.want, got, "decode(%s)", tt.args)
 	}
 }
